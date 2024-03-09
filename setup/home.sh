5fcfb71630364238070fdb6c4fdcf76c952619b4
0x1eef <0x1eef@protonmail.com>
2024-03-09 07:51:20 -0300

    Update home.sh

--- setup/home.sh
+++ setup/home.sh
@@ -2,22 +2,23 @@
 
 create_home()
 {
+    ##
+    # Copy
     cp -Rf src/home/_blocklist/* /home/_blocklist/
 
+    ##
+    # Ownership
     chown -R _blocklist:_blocklist /home/_blocklist/
-    chmod -R u=rwX,g=rX,o= /home/_blocklist/
-
     chown -R root:_blocklist /home/_blocklist/libexec/
-    chmod -R u=rx,g=rx,o= /home/_blocklist/libexec/
-
     chown -R root:_blocklist /home/_blocklist/bin/
-    chmod -R u=rx,g=rx,o= /home/_blocklist/bin/
 
-    chown -R _blocklist:_blocklist /home/_blocklist/blocklists/
-    chmod -R u=rwx,g=rx,o= /home/_blocklist/blocklists/
-
-    chown -R _blocklist:_blocklist /home/_blocklist/reloads/
-    chmod -R u=rwx,g=rx,o= /home/_blocklist/reloads/
+    ##
+    # Permissions
+    chmod u=rwx,g=rx,o= /home/_blocklist/
+    find /home/_blocklist/ -type d -exec chmod u=rwx,g=rx,o= {} +
+    find /home/_blocklist/ -type f -exec chmod u=rw,g=r,o= {} +
+    find /home/_blocklist/libexec/ -type f -exec chmod u=rx,g=rx,o= {} +
+    find /home/_blocklist/bin/ -type f -exec chmod u=rx,g=rx,o= {} +
 
     echo "[ok] /home/_blocklist/ setup."
 }
