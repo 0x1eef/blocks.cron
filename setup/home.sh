9b19be31b651d4cf34ff7a532e8a8de68fd52b16
0x1eef <0x1eef@protonmail.com>
2024-03-10 06:03:16 -0300

    Add OpenBSD support
    'date -I' is not supported on OpenBSD.
    Add 'today' function.

--- setup/home.sh
+++ setup/home.sh
@@ -5,10 +5,12 @@ create_home()
     ##
     # Copy
     cp -Rf src/home/_blocklist/* /home/_blocklist/
+    cp -Rf src/home/_blocklist/.* /home/_blocklist/
 
     ##
     # Ownership
     chown -R _blocklist:_blocklist /home/_blocklist/
+    chown -R root:_blocklist /home/_blocklist/functions/
     chown -R root:_blocklist /home/_blocklist/libexec/
     chown -R root:_blocklist /home/_blocklist/bin/
 
@@ -17,6 +19,7 @@ create_home()
     chmod u=rwx,g=rx,o= /home/_blocklist/
     find /home/_blocklist/ -type d -exec chmod u=rwx,g=rx,o= {} +
     find /home/_blocklist/ -type f -exec chmod u=rw,g=r,o= {} +
+    find /home/_blocklist/functions/ -type f -exec chmod u=rx,g=rx,o= {} +
     find /home/_blocklist/libexec/ -type f -exec chmod u=rx,g=rx,o= {} +
     find /home/_blocklist/bin/ -type f -exec chmod u=rx,g=rx,o= {} +
 
