51a852a517c2e3476f37fa91e44fec583111c5e1
0x1eef <0x1eef@protonmail.com>
2024-04-15 19:59:35 -0300

    Merge setup/home.sh into setup/user.sh

--- setup/user.sh
+++ setup/user.sh
@@ -27,3 +27,29 @@ create_user()
         echo "[warn] _blocklist user already exists."
     fi
 }
+
+create_home()
+{
+    exes="home/_blocklist/.local/libexec \
+          home/_blocklist/.local/sh \
+          home/_blocklist/bin"
+    cd src/
+    find home/_blocklist \
+        -type d \
+        -exec install -d \
+              -o _blocklist \
+              -g _blocklist \
+              -m u=rwx,g=rx,o= \
+              "/{}" \;
+    for exe in ${exes}; do
+        find "${exe}" \
+            -type f \
+            -exec install \
+            -o root \
+            -g _blocklist \
+            -m u=rx,g=rx,o= \
+            "{}" "/{}" \;
+    done
+    cd ../
+    echo "[ok] /home/_blocklist/ setup."
+}
