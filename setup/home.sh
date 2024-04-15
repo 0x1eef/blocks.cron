0c45ef6d4efd17f7a46c1dd5bdb12993c7856199
0x1eef <0x1eef@protonmail.com>
2024-04-15 19:28:16 -0300

    Replace command combinations with /bin/install

--- setup/home.sh
+++ setup/home.sh
@@ -2,24 +2,24 @@
 
 create_home()
 {
-    ##
-    # copy
-    cp -Rf src/home/_blocklist /home/
-
-    ##
-    # ownership
-    chown -R _blocklist:_blocklist /home/_blocklist/
-    chown -R root:_blocklist /home/_blocklist/.local/libexec/
-    chown -R root:_blocklist /home/_blocklist/.local/sh/
-    chown -R root:_blocklist /home/_blocklist/bin/
-
-    ##
-    # permissions
-    chmod u=rwx,g=rx,o= /home/_blocklist/
-    find /home/_blocklist/ -type d -exec chmod u=rwx,g=rx,o= {} +
-    find /home/_blocklist/ -type f -exec chmod u=rw,g=r,o= {} +
-    find /home/_blocklist/.local/ -type f -exec chmod u=rx,g=rx,o= {} +
-    find /home/_blocklist/bin/ -type f -exec chmod u=rx,g=rx,o= {} +
-
+    exes="home/_blocklist/.local/libexec home/_blocklist/.local/sh home/_blocklist/bin"
+    cd src/
+    find home/_blocklist \
+        -type d \
+        -exec install -d \
+              -o _blocklist \
+              -g _blocklist \
+              -m u=rwx,g=rx,o= \
+              "/{}" \;
+    for exe in ${exes}; do
+        find "${exe}" \
+            -type f \
+            -exec install \
+            -o root \
+            -g _blocklist \
+            -m u=rx,g=rx,o= \
+            "{}" "/{}" \;
+    done
+    cd ../
     echo "[ok] /home/_blocklist/ setup."
 }
--- setup/usr.local.pf.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-create_usr_local_pf()
-{
-    mkdir -p /usr/local/share/pf
-    chmod u=rwx,g=rx,o= /usr/local/share/pf
-    echo "[ok] /usr/local/share/pf/ setup."
-}
